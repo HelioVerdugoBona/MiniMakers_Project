a9f9b4908beea47e57649060e4e7bcbbb5203e80
VictoCortesCortes <167753932+VictoCortesCortes@users.noreply.github.com>
2024-10-29 19:43:27 +0100

    Actualizar el nombre del json al guardarlo o leerlo

--- Simon_Dice/SimonDiceFinal/SimonDiceFinal/app/src/main/java/com/example/simondicefinal/FilesManager.kt
+++ Simon_Dice/SimonDiceFinal/SimonDiceFinal/app/src/main/java/com/example/simondicefinal/FilesManager.kt
@@ -12,14 +12,14 @@ class FilesManager {
     companion object {
         // Comprueba si el archivo de las partidas existe y si tiene contenido dentro
         fun comprovarArchivo(context: Context): Boolean {
-            val jsonFilePath = context.getFilesDir().toString() + "/partidas.json"
+            val jsonFilePath = context.getFilesDir().toString() + "/partidasSimon.json"
             val jsonFile = File(jsonFilePath)
             return jsonFile.exists() && jsonFile.length() != 0L
         }
 
         // Lee el archivo de las partidas guardadas y lo convierte un una MutableList
         fun obtenerPartidas(context: Context): MutableList<Partida> {
-            val jsonFilePath = context.getFilesDir().toString() + "/partidas.json"
+            val jsonFilePath = context.getFilesDir().toString() + "/partidasSimon.json"
             val jsonFile = FileReader(jsonFilePath)
             val listPartidasType = object: TypeToken<MutableList<Partida>>() {}.type
             val partidas: MutableList<Partida> = Gson().fromJson(jsonFile, listPartidasType)
@@ -28,7 +28,7 @@ class FilesManager {
 
         // Recive una MutableList y guarda los datos en el archivo de partidas guardadas
         fun guardarPartidas(context: Context, partidas: List<Partida>) {
-            val jsonFilePath = context.getFilesDir().toString() + "/partidas.json"
+            val jsonFilePath = context.getFilesDir().toString() + "/partidasSimon.json"
             val jsonFile = FileWriter(jsonFilePath)
             var gson = Gson()
             var jsonElement = gson.toJson(partidas)
