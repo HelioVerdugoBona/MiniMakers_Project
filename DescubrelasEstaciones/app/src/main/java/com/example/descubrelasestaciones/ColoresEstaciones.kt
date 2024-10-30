2e62ee3735510e7312e5807568b476f152a7b153
HelioVerdugoBona <hverdugob2425@politecnics.barcelona>
2024-10-30 20:08:37 +0100

    Añadido eliminar items

--- DescubrelasEstaciones/app/src/main/java/com/example/descubrelasestaciones/ColoresEstaciones.kt
+++ DescubrelasEstaciones/app/src/main/java/com/example/descubrelasestaciones/ColoresEstaciones.kt
@@ -37,74 +37,87 @@ class ColoresEstaciones: AppCompatActivity() {
         val itemEstacionesList = findViewById<RecyclerView>(R.id.recyclerViewColores)
         val arrayEstacionesList = findViewById<RecyclerView>(R.id.recylerViewEstaciones)
 
-        setupRecyclerView(itemEstacionesList, itemsEstaciones)
-        setupRecyclerView(arrayEstacionesList, arrayEstaciones)
+        setupRecyclerView(itemEstacionesList, itemsEstaciones,arrayEstacionesList,arrayEstaciones)
     }
 
     private fun setupRecyclerView(
-        recyclerView: RecyclerView,
-        itemsEstaciones: MutableList<ItemEstaciones>
+        recyclerView1: RecyclerView,
+        itemsEstaciones: MutableList<ItemEstaciones>,
+        recyclerView2: RecyclerView,
+        arrayEstaciones: MutableList<ItemEstaciones>
     ) {
-        val adapter = ItemsEstacionesAdapter(this, itemsEstaciones)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.layoutManager = GridLayoutManager(this,4)
-        recyclerView.adapter = adapter
+        val adapterItem = ItemsEstacionesAdapter(this, itemsEstaciones)
+        recyclerView1.layoutManager = LinearLayoutManager(this)
+        recyclerView1.layoutManager = GridLayoutManager(this,4)
+        recyclerView1.adapter = adapterItem
 
-        println("El ID del RecyclerView es: ${recyclerView.id}")
+
+        val adapterEstacion = ItemsEstacionesAdapter(this, arrayEstaciones)
+        recyclerView2.layoutManager = LinearLayoutManager(this)
+        recyclerView2.layoutManager = GridLayoutManager(this,4)
+        recyclerView2.adapter = adapterEstacion
 
         val itemEstacion = itemsEstaciones[0]
-        if (itemEstacion.nombreItem != "Verano") {
-            adapter.setOnLongClickListener { view, i ->
-                val item = itemsEstaciones[i]
+        adapterItem.setOnLongClickListener { view, i ->
+            val item = itemsEstaciones[i]
 
-                // Inicia el arrastre con una sombra personalizada
-                val dragData = ClipData.newPlainText("id", item.id)
-                val dragShadow = CustomDragShadowBuilder(view) // Usar la sombra personalizada
-                view.startDragAndDrop(dragData, dragShadow, view, 0)
-            }
+            // Inicia el arrastre con una sombra personalizada
+            val dragData = ClipData.newPlainText("id", item.id)
+            val dragShadow = CustomDragShadowBuilder(view) // Usar la sombra personalizada
+            view.startDragAndDrop(dragData, dragShadow, view, 0)
         }
 
-        if (itemEstacion.nombreItem == "Verano") {
-            recyclerView.setOnDragListener { _, event ->
-                when (event.action) {
-                    DragEvent.ACTION_DROP -> {
-                        // val draggedItem = event.localState as ItemEstaciones
-                        // Obtener el atributo del ítem arrastrado
-                        val draggedAttribute = event.clipData.getItemAt(0).text.toString()
-                         println("El ID del draggedAttribute es: $draggedAttribute")
 
-                        // Encuentra el ítem de destino (donde se soltó el arrastre)
-                        val x = event.x
-                        val y = event.y
-                        val viewUnder = recyclerView.findChildViewUnder(x, y)
-                        val targetPosition = if (viewUnder != null) {
-                            recyclerView.getChildAdapterPosition(viewUnder)
-                        } else {
-                            RecyclerView.NO_POSITION
-                        }
-                        println("El targetPosition es: $targetPosition")
+        recyclerView2.setOnDragListener { _, event ->
+            when (event.action) {
+                DragEvent.ACTION_DROP -> {
+                    // val draggedItem = event.localState as ItemEstaciones
+                    // Obtener el atributo del ítem arrastrado
+                    val draggedAttribute = event.clipData.getItemAt(0).text.toString()
+                     println("El ID del draggedAttribute es: $draggedAttribute")
+
+                    // Encuentra el ítem de destino (donde se soltó el arrastre)
+                    val x = event.x
+                    val y = event.y
+                    val viewUnder = recyclerView2.findChildViewUnder(x, y)
+                    val targetPosition = if (viewUnder != null) {
+                        recyclerView2.getChildAdapterPosition(viewUnder)
+                    } else {
+                        RecyclerView.NO_POSITION
+                    }
+                    println("El targetPosition es: $targetPosition")
 
 
-                        if (targetPosition != RecyclerView.NO_POSITION) {
-                            val targetItem = arrayEstaciones[targetPosition]
+                    if (targetPosition != RecyclerView.NO_POSITION) {
+                        val targetItem = arrayEstaciones[targetPosition]
 
-                            // Compara los atributos
-                            if (draggedAttribute == targetItem.id) {
-                                Log.d("DragAndDrop", "Atributos coinciden")
-                            } else {
-                                Log.d("DragAndDrop", "Atributos no coinciden")
+                        // Compara los atributos
+                        if (draggedAttribute == targetItem.id) {
+                            val iterator = itemsEstaciones.iterator()
+                            while (iterator.hasNext()) {
+                                val item = iterator.next()
+                                if (draggedAttribute == item.id) {
+                                    iterator.remove() // Elimina usando el iterador
+                                    adapterItem.notifyDataSetChanged()
+                                    break // Salir del bucle después de eliminar
+                                }
                             }
+                            Log.d("DragAndDrop", "Atributos coinciden")
+
+                        } else {
+                            Log.d("DragAndDrop", "Atributos no coinciden")
                         }
-                        true
                     }
-                    DragEvent.ACTION_DRAG_ENDED -> {
-                        // Restablecer la opacidad del ítem arrastrado
-                        adapter.clearAlpha() // Restablecer la opacidad de todos los ítems
-                        true
-                    }
-                else -> true
+                    true
                 }
+                DragEvent.ACTION_DRAG_ENDED -> {
+                    // Restablecer la opacidad del ítem arrastrado
+                    adapterEstacion.clearAlpha() // Restablecer la opacidad de todos los ítems
+                    true
+                }
+            else -> true
             }
         }
     }
+
 }
