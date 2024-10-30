7e619976969af831c21b0d9f19e492615be69af4
HelioVerdugoBona <hverdugob2425@politecnics.barcelona>
2024-10-30 20:19:18 +0100

    Cambio Botones del movil

--- DescubrelasEstaciones/app/src/main/java/com/example/descubrelasestaciones/MainActivity.kt
+++ DescubrelasEstaciones/app/src/main/java/com/example/descubrelasestaciones/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.descubrelasestaciones
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.AdapterView
 import android.widget.GridView
 import androidx.appcompat.app.AppCompatActivity
@@ -23,6 +24,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_FULLSCREEN or
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                )
 
         val gridOfAvatares = findViewById<GridView>(R.id.ArrayAvatares)
 
