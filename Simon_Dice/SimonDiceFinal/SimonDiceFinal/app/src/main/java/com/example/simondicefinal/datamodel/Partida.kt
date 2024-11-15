cbec12e53d2ee2d00a90ccd389077136902c6c70
VictoCortesCortes <167753932+VictoCortesCortes@users.noreply.github.com>
2024-11-15 19:58:42 +0100

    Cambiar el atributo del tiempo de un string a un int

--- Simon_Dice/SimonDiceFinal/SimonDiceFinal/app/src/main/java/com/example/simondicefinal/datamodel/Partida.kt
+++ Simon_Dice/SimonDiceFinal/SimonDiceFinal/app/src/main/java/com/example/simondicefinal/datamodel/Partida.kt
@@ -1,3 +1,3 @@
 package com.example.simondicefinal.datamodel
 
-class Partida (val nombre: String, val rondas: Int, val tiempo: String, val fecha: String)
+class Partida (val nombre: String, val rondas: Int, val tiempo: Int, val fecha: String)
